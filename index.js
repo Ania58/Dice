8031c0bf8fc3e2fb9ad11425ede3221478c11670
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-01-01 15:25:53 +0100

    Add event listener to check where the issue lies

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-getRandomNumber = () => {
-    return Math.floor(Math.random() * 6) + 1;
-}
-
-let randomImage1 = getRandomNumber();
-let randomImage2 = getRandomNumber();
-
-
-document.querySelector(".img1").setAttribute("src", `./images/dice${randomImage1}.png`);
-
-document.querySelector(".img2").setAttribute("src",`./images/dice${randomImage2}.png`);
-
-whoWon = () => {
-    if (randomImage1 > randomImage2) {
-        return "Player 1 Wins";
-    } else if (randomImage1 < randomImage2) {
-        return "Player 2 Wins";
-    } else {
-        return "It's a draw";
+document.addEventListener("DOMContentLoaded", () => {
+    getRandomNumber = () => {
+        return Math.floor(Math.random() * 6) + 1;
     }
-}
-
-let result = whoWon();
-document.querySelector("h1").textContent = result;
+    
+    let randomImage1 = getRandomNumber();
+    let randomImage2 = getRandomNumber();
+    
+    
+    document.querySelector(".img1").setAttribute("src", `./images/dice${randomImage1}.png`);
+    
+    document.querySelector(".img2").setAttribute("src",`./images/dice${randomImage2}.png`);
+    
+    whoWon = () => {
+        if (randomImage1 > randomImage2) {
+            return "Player 1 Wins";
+        } else if (randomImage1 < randomImage2) {
+            return "Player 2 Wins";
+        } else {
+            return "It's a draw";
+        }
+    }
+    
+    let result = whoWon();
+    document.querySelector("h1").textContent = result;
+});
