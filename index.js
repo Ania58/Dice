3f2853c5801d6ef02c2d6b444d780022ebc9ebf1
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-01-01 17:59:22 +0100

    Add the throw class when the button is clicked and timeout of 1 second when the image is selected and the class removed

--- index.js
+++ index.js
@@ -5,25 +5,34 @@ getRandomNumber = () => {
 const buttonClick = document.getElementById("btn");
 
 buttonClick.addEventListener("click", function() {
-let randomImage1 = getRandomNumber();
-let randomImage2 = getRandomNumber();
 
+    const dice1 = document.querySelector(".img1");
+    const dice2 = document.querySelector(".img2");
 
-document.querySelector(".img1").setAttribute("src", `./images/dice${randomImage1}.png`);
-
-document.querySelector(".img2").setAttribute("src",`./images/dice${randomImage2}.png`);
-
-whoWon = () => {
-    if (randomImage1 > randomImage2) {
-        return "Player 1 Wins";
-    } else if (randomImage1 < randomImage2) {
-        return "Player 2 Wins";
-    } else {
-        return "It's a draw";
+    dice1.classList.add("throw");
+    dice2.classList.add("throw");
+    
+    let randomImage1 = getRandomNumber();
+    let randomImage2 = getRandomNumber();
+    
+    setTimeout(() => {
+        document.querySelector(".img1").setAttribute("src", `./images/dice${randomImage1}.png`);
+        document.querySelector(".img2").setAttribute("src",`./images/dice${randomImage2}.png`);
+        dice1.classList.remove("throw");
+        dice2.classList.remove("throw");
+    }, 1000);
+    
+    whoWon = () => {
+        if (randomImage1 > randomImage2) {
+            return "Player 1 Wins";
+        } else if (randomImage1 < randomImage2) {
+            return "Player 2 Wins";
+        } else {
+            return "It's a draw";
+        }
     }
-}
-
-let result = whoWon();
-document.querySelector("h1").textContent = result;
+    
+    let result = whoWon();
+    document.querySelector("h1").textContent = result;
 
 });
