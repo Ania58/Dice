fc61ca4a8cb6c1184979f62d0f0bf30e3b876fe3
Anna Maria Heliasz <annemaryheliasz@gmail.com>
2025-01-01 16:29:09 +0100

    Change the index.js to roll the dices when clicked, and not when refreshed

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 getRandomNumber = () => {
     return Math.floor(Math.random() * 6) + 1;
 }
-    
+
+const buttonClick = document.getElementById("btn");
+
+buttonClick.addEventListener("click", function() {
 let randomImage1 = getRandomNumber();
 let randomImage2 = getRandomNumber();
-    
-    
+
+
 document.querySelector(".img1").setAttribute("src", `./images/dice${randomImage1}.png`);
-    
+
 document.querySelector(".img2").setAttribute("src",`./images/dice${randomImage2}.png`);
-    
+
 whoWon = () => {
     if (randomImage1 > randomImage2) {
         return "Player 1 Wins";
@@ -19,6 +22,8 @@ whoWon = () => {
         return "It's a draw";
     }
 }
-    
+
 let result = whoWon();
 document.querySelector("h1").textContent = result;
+
+});
